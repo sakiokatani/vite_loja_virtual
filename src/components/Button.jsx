701cc322b72fdfce7af242942290d7728680f467
Saki Okatani <saki.okatani1@gmail.com>
2023-07-06 02:12:39 -0300

    butoon atualizado

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,17 +2,31 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 import buttonStyle from '../styles/button.css'
+import ProductDetailPage from './ProductDetailPage'
 
 const Button=(prop)=>{
+
+    const onClickEvent= ()=>{
+        if(prop.btnID === '1'){
+            console.log('button 1')
+            //trocar texto header e pág de produto//
+        }
+        if(prop.btnID === '2'){
+            console.log('button 2')
+        }
+    }
+
     return(
         <>
         <div className= "button-container">
-           <a href="#"><button className="reusableBtn" onclick="#" >{prop.prop1}</button></a> 
+           <button className="reusableBtn" onClick={onClickEvent}>{prop.label}</button>
         </div>
         </>
     )
 }
 
-
+Button.propTypes={
+    prop1: PropTypes.string,
+}
 
 export default Button;
