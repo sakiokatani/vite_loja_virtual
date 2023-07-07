333ea749a61a0fdf788ffeef375940897f8644a1
Saki Okatani <saki.okatani1@gmail.com>
2023-07-07 00:51:17 -0300

    footer

---
+++ src/components/ProductDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const ProductDetailPage = ()=>{
+    return(
+        <>
+        <p>This is the product detail page</p>
+        </>
+    )
+}
+
+export default ProductDetailPage;
